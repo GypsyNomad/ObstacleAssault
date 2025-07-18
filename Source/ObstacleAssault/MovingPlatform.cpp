e7759d8a08b35885db499ec2ea4ff3fa7f26aa43
GypsyNomad <a.b.s960@gmail.com>
2025-07-18 17:29:51 +0500

    fix header order, remove unnecessary comments

--- Source/ObstacleAssault/MovingPlatform.cpp
+++ Source/ObstacleAssault/MovingPlatform.cpp
@@ -1,17 +1,12 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+#include "MovingPlatform.h"
 
 #include <tuple>
 
-#include "MovingPlatform.h"
-
-// Sets default values
 AMovingPlatform::AMovingPlatform()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 }
 
-// Called when the game starts or when spawned
 void AMovingPlatform::BeginPlay()
 {
 	Super::BeginPlay();
@@ -56,7 +51,6 @@ void AMovingPlatform::MovePlatform(float DeltaTime)
 	SetActorLocation(newLocation);
 }
 
-// Called every frame
 void AMovingPlatform::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
